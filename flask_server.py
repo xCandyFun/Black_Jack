c0b3d13c413652737854f2502bfba5a176b3d8ec
Simon T. <simtho0624@gmail.com>
2025-04-08 16:33:28 +0200

    add basic flask

---
+++ flask_server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/normal_game')
+def normal_black_jack():
+    return render_template("normal_black_jack.html")
+
+@app.route('/special_game')
+def special_black_jack():
+    return render_template("special_black_jack.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
